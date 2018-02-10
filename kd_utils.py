ab13f1eac99cde3822061aa5cb42b2004359b80f
Trey Wenger <tvwenger@gmail.com>
2018-02-10 16:15:31 -0500

    added correct_vlsr

--- kd_utils.py
+++ kd_utils.py
@@ -5,6 +5,7 @@ kd_utils.py
 Utility functions for rotcurve_kd.py, pdf_kd.py, and rotation curves.
 
 2017-04-12 Trey V. Wenger
+2018-02-10 Trey V. Wenger added correct_vlsr
 """
 
 import time
@@ -290,3 +291,103 @@ def pool_wait(result,num_items,chunksize):
     time_s = int(run_time-3600.*time_h-60.*time_m)    
     print("Runtime: {0:02}h {1:02}m {2:02}s".\
           format(time_h,time_m,time_s))
+
+def correct_vlsr(glong, glat, vlsr, e_vlsr,
+                 Ustd=10.27, Vstd=15.32, Wstd=7.74,
+                 Usun=10.5, e_Usun=1.7, Vsun=14.4, e_Vsun=6.8,
+                 Wsun=8.9, e_Wsun=0.9):
+    """
+    Return the "corrected" LSR velocity by updating the IAU-defined
+    solar motion components (Ustd,Vstd,Wstd) to newly-measured
+    values (Usun,Vsun,Wsun).
+    Also computes the new LSR velocity uncertainty including the
+    uncertainties in the newly-measured values (e_Usun,e_Vsun,e_Wsun). 
+
+    Parameters:
+      glong : scalar or 1-D array
+              Galactic longitude (deg). If it is an array, it must
+              have the same size as glat, vlsr, and e_vlsr.
+      glat : scalar or 1-D array
+             Galactic latitude (deg). If it is an array, it
+             must have the same size as glong, vlsr, and e_vlsr.
+      vlsr : scalar or 1-D array
+             Measured LSR velocity (km/s). If it is an array, it
+             must have the same size as glong, glat, and e_vlsr.
+      e_vlsr : scalar or 1-D array
+               Uncertainty on measured LSR velocity (km/s). If it is 
+               an array, it must have the same size as glong, glat, 
+               and vlsr.
+      Ustd,Vstd,Wstd : scalar (optional)
+                       IAU-defined solar motion parameters (km/s).
+      Usun,Vsun,Wsun : scalar (optional)
+                       Newly measured solar motion parameters (km/s).
+                       Defaults are from Reid et al. (2014)
+      e_Usun,e_Vsun,e_Wsun : scalar (optional)
+                       Newly measured solar motion parameter
+                       uncertainties (km/s).
+                       Defaults are from Reid et al. (2014)
+
+    Returns: (new_vlsr,e_new_vlsr)
+      new_vlsr : scalar or 1-D array
+                 Re-computed LSR velocity. Same shape as vlsr.
+      e_vlsr : scalar or 1-D array
+               Re-computed LSR velocity uncertainty. Same shape as 
+               e_vlsr.
+
+    Raises:
+      ValueError : if glong, glat, vlsr, and e_vlsr are not 1-D; or
+                   if glong, glat, vlsr, and e_vlsr are arrays and 
+                   not the same size
+    """
+    #
+    # check inputs
+    #
+    # convert scalar to array if necessary
+    glong_inp, glat_inp, vlsr_inp, e_vlsr_inp = \
+      np.atleast_1d(glong, glat, vlsr, e_vlsr)
+    # check shape of inputs
+    if (glong_inp.ndim != 1 or glat_inp.ndim != 1 or
+        vlsr_inp.ndim != 1 or e_vlsr_inp.ndim != 1):
+        raise ValueError("glong, glat, vlsr, and e_vlsr must be 1-D")
+    if glong_inp.size != 1 and (glong_inp.size != glat_inp.size or
+                                glong_inp.size != vlsr_inp.size or
+                                glong_inp.size != e_vlsr_inp.size):
+        raise ValueError("glong, glat, vlsr, and e_vlsr must have same size")
+    #
+    # Useful values
+    #
+    cos_glong = np.cos(np.deg2rad(glong_inp))
+    sin_glong = np.sin(np.deg2rad(glong_inp))
+    cos_glat = np.cos(np.deg2rad(glat_inp))
+    sin_glat = np.sin(np.deg2rad(glat_inp))
+    #
+    # Compute heliocentric velocity by subtracting IAU defined solar
+    # motion components
+    #
+    U_part = Ustd*cos_glong
+    V_part = Vstd*sin_glong
+    W_part = Wstd*sin_glat
+    UV_part = (U_part+V_part)*cos_glat
+    v_helio = vlsr_inp - UV_part - W_part
+    #
+    # Compute corrected VLSR
+    #
+    U_part = Usun*cos_glong
+    V_part = Vsun*sin_glong
+    W_part = Wsun*sin_glat
+    UV_part = (U_part+V_part)*cos_glat
+    new_vlsr = v_helio + UV_part + W_part
+    #
+    # Compute corrected LSR velocity uncertainty
+    #
+    U_part = (e_Usun*cos_glong*cos_glat)**2.
+    V_part = (e_Vsun*sin_glong*cos_glat)**2.
+    W_part = (e_Wsun*sin_glat)**2.
+    e_new_vlsr = np.sqrt(e_vlsr_inp**2.+U_part+V_part+W_part)
+    #
+    # Convert back to scalar if necessary
+    #
+    if glong_inp.size == 1:
+        return new_vlsr[0],e_new_vlsr[0]
+    else:
+        return new_vlsr,e_new_vlsr
